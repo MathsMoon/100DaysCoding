217f7b5ed85098a4db525149512be5badc664ba7
Matheus A <matheusalvessouza1@hotmail.com>
2024-03-06 20:42:19 -0300

    Primeiro dia de aula
    Tenho que voltar a aprender a usar isso

---
+++ Aulas de java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Aqui dou oi para minha apliacação no Github");
+        System.out.println("Olá");
+    }
+}
