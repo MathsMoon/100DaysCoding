183944dbc9e05449c8340a11f0be0afba24f80d7
Matheus A <matheusalvessouza1@hotmail.com>
2024-03-09 23:39:25 -0300

    Update do dia 09/03
    Precisa estudar o caso do Switch, e entender os loops e sua estruturação. Usando em uma só aplicação todos os dados primitivos de uma só vez.

--- Aulas de java/src/App.java
+++ Aulas de java/src/App.java
@@ -1,6 +1,40 @@
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Aqui dou oi para minha apliacação no Github");
-        System.out.println("Olá");
-    }
+        int nota1 = 7, nota2 = 7, nota3 = 5, nota4 = 8;
+        float media = (nota1 + nota2 + nota3 + nota4)/4;
+
+        System.out.println("Sua média é: " + media);
+
+        if (media >= 7) {
+            System.out.println("Aprovado");
+        } else if ( media > 5.9 && media < 7 ) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+        
+        int pontoextra = (int) (media/7);
+
+        /* 
+        A ideia aqui é para o pontoextra ser o que falta para o aluno sair da recuperação
+         então reveja como isso é possível amanhã.
+
+
+        switch (pontoextra) {
+            case 1:
+                System.out.println("Para passar precisa de 1 ponto");
+                break;
+            case 2:
+                System.out.println("Para passar precisa de 2 pontos");
+                break;
+            default:
+                System.out.println("Não tem ponto suficiente");
+                break;
+                */
+        }
+    }   
 }
