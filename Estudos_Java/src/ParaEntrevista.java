732f9b2e50c38c38115ccc8877345b0aa4531e81
Matheus A <matheusalvessouza1@hotmail.com>
2024-04-03 22:34:54 -0300

    Update dia 03_04
    Preciso tentar fazer essa atividade da entrevista e continuar as aulas do CFB

---
+++ Estudos_Java/src/ParaEntrevista.java
@@ -0,0 +1,45 @@
+public class ParaEntrevista {
+
+    /*
+
+    Olá, seja bem-vindo ao meu repositório de estudos, abaixo resolvi as questões para a entrevista usando métodos 
+    no java, que é a linguagem que estou aprendendo. 
+    Se quiser ver meu progresso nos estudos basta ver o ReadMe da aba principal (Meu diário de programador do desafio 100DaysCode) 
+    ou entrar nas pastas dentro do src.
+    
+    */
+
+
+    public static void ProcessoLoop() {//1 - Processo de Loop
+        //Definindo parâmetros para o Loop
+        int indice = 13, soma = 0;
+
+        for (int k = 0; k<indice; k++) {
+            soma++;
+        }
+        System.out.printf("A soma final ficou: %d", soma);
+    }
+
+    public static void Fibonnaci(int n) {//2 - Código para Fibonnaci.
+        
+    }
+
+    public static void IncrementoNum() {//3 - Lógica de incremento.
+
+    }
+
+    public static void LogicLamp() {   //4 - Lógica para Lâmpadas.
+
+    }
+
+    public static void ReversingString(String n) {//5 - Inversão de String.
+        
+    }
+
+    public static void main (String[] args) {
+       //Métodos para Mostrar os resultados: 
+        ProcessoLoop();
+
+
+    }
+}
