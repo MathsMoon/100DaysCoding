1f91a5a50728fc0e740bcf0fd1e25beb4ec63539
Matheus A <matheusalvessouza1@hotmail.com>
2024-04-05 22:58:30 -0300

    Update 05_04
    Atualização diária

---
+++ Estudos_Java/src/Atividades_avulso/Secretaria.java
@@ -0,0 +1,15 @@
+package Atividades_avulso;
+
+public class Secretaria {
+    /*Você foi contratado para desenvolver um programa que irá gerenciar uma lista
+    de tarefas diárias de um usuário. O programa deve permitir ao usuário adicionar novas tarefas,
+    remover tarefas existentes, listar todas as tarefas pendentes.
+
+    1 - O programa deve usar um array para armazenar as tarefas do usuário.
+    2 - O usuário deve ser capaz de adicionar uma nova tarefa à lista
+    3 - O usuário deve ser capaz de remover uma tarefa existente da lista.
+    4 - O programa deve exibir todas as tarefas na lista
+    
+    Crie um Menu para o usuário escolher a opção desejada e ele
+    quando realizar a operação o sistema deve retornar ao menu principal*/
+}
--- Estudos_Java/src/CFB_CursoJava/desafio.java
+++ Estudos_Java/src/CFB_CursoJava/desafio.java
@@ -4,68 +4,78 @@ import java.util.Scanner;
 
 public class desafio {
 
-    static String Nome;
-    static Scanner scan = new Scanner(System.in);
-    final static int TAM = 5;
-    static int[] respostas = new int[TAM];
-    static int[] gabarito = {2, 4, 3, 1, 5};
+    //Definindo atributos para a prova
+    static String Nome; //Nome Aluno
+    static Scanner scan = new Scanner(System.in); //Objeto de Scanner
+    final static int TAM = 5; //Quantidade de questões na prova
+    static int[] respostas = new int[TAM];  //Respostas das questões
+    static int[] gabarito = {1, 3, 1, 2, 4}; //Gabarito da prova
     
-    public static void Prova() {
+    public static void Prova() { //Método para a prova em si
+        //Introdução:
         System.out.printf("\nSeja bem-vindo a prova %s.\nAbaixo temos 5 perguntas, cada questão vale 2 pontos, Boa Sorte.", Nome);
-        System.out.println("\n--------------------------------");
 
-        System.out.println("A - Quanto é 2 + 2 ?\n1) 2\n2) 4\n3) 5\n4) 8\n5)10");
-        respostas[0] = scan.nextInt();
-        System.out.println("\n--------------------------------");
-        System.out.println("B - Quanto é 2 + 2 ?\n1) 2\n2) 4\n3) 5\n4) 8\n5)10");
-        respostas[1] = scan.nextInt();
-        System.out.println("\n--------------------------------");
-        System.out.println("C - Quanto é 2 + 2 ?\n1) 2\n2) 4\n3) 5\n4) 8\n5)10");
-        respostas[2] = scan.nextInt();
-        System.out.println("\n--------------------------------");
-        System.out.println("D - Quanto é 2 + 2 ?\n1) 2\n2) 4\n3) 5\n4) 8\n5)10");
-        respostas[3] = scan.nextInt();
-        System.out.println("\n--------------------------------");
-        System.out.println("E - Quanto é 2 + 2 ?\n1) 2\n2) 4\n3) 5\n4) 8\n5)10");
-        respostas[4] = scan.nextInt();
-        System.out.println("\n--------------------------------");
+        //Criando as Perguntas e Alternativas
+        String[] perguntas = {
+            "Qual a Maior estrela do Sistema Solar?",
+            "Qual a Primeira Letra do Nosso Alfabeto?",
+            "Qual é a linguagem usada para criar este Programa?",
+            "No combate quem ganharia? Dante Will (Modo Aranha) ou Toshi modo demônio?",
+            "Qual melhor Cantor da Atualidade?"
+        };
+        
+        String[] alternativas = {
+            "1)Sol   |   2) Lua   |   3) Marte",
+            "1)C   | 2)   L   |   3) A",
+            "1)Java   | 2)   Lua   | 3)   C++",
+            "1)Chat no Prime   |   2) Loud Tetracampeã CBLOL",
+            "1) MC VV\n2) Yun Li\n3) O de óculos do JogandoFoddaci\n4) Todas acima"
+        };
 
-        Correcao(respostas);
+
+        //Realizando um Loop para as Perguntas
+        for(int i = 0; i<TAM; i++) {
+            System.out.println("\n--------------------------------");
+            System.out.printf("Pergunta %d%n", i+1);
+            System.out.printf("%s%n", perguntas[i]);
+            System.out.printf("%s%n", alternativas[i]);
+            respostas[i] = scan.nextInt();
+        }
+
+       //Correção da prova 
+        Correcao();
     }
 
 
-    public static void Correcao(int[] a) {
-        int[] b = a;
-        int nota = 0;
+    public static void Correcao() {//Método de correção com peso 2
+        int acertos = 0;
 
-        for (int i = 0; i<TAM; i++) {
-            if(b[i] == respostas[i]){
-            nota++;
+        if(Arrays.equals(respostas, gabarito)) {//Se a resposta é igual ao gabarito, acertou tudo
+            acertos = 5;
+        } else {
+            for(int i = 0; i<TAM; i++) {//Se não, passe por cada membro da Array dos dois e compare 1 por 1.
+                if (respostas[i] == gabarito[i]) {
+                    acertos++;
+                }
             }
         }
+        
+        //Criando a varíavel que recebe resultado com o peso devido
+        int total = acertos * 2;
 
-        System.out.println(nota);
-        System.exit(0);
-
-        switch (nota) {
-            case 0, 1, 2:
-                System.out.printf("%s você tirou menos que 6, você foi reprovado", Nome);
-            break;
-
-            case 3:
-                System.out.printf("%s você tirou 6, você está de recuperação.", Nome);
-            break;
-
-            default:
-                System.out.printf("Parabéns %s você passou.", Nome);
-            break;
+        if (total > 6) {
+            System.out.printf("Parabéns %s sua nota foi: %d, você passou.", Nome, total );
+        } else if(total == 6 ) {
+            System.out.printf("%s sua nota foi: %d, você está de recuperação.", Nome, total);
+        } else {
+            System.out.printf("%s sua nota foi de: %d, você foi reprovado", Nome, total);
         }
     }
 
     public static void main(String[] args) {
         //Inserção do Nome
-        System.out.print("Digite seu Nome:");
-        Nome = scan.nextInt();
+        System.out.print("Digite seu Nome: ");
+        Nome = scan.nextLine();
 
         //Começo da prova
         Prova();
