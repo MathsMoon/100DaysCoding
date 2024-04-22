0df3b4268931e43139aa6077924f71fe185695ef
Matheus A <matheusalvessouza1@hotmail.com>
2024-04-21 23:49:50 -0300

    Update 21_04
    Faça, FAÇA ATIVIDADE IMEDIATAMENTE

---
+++ Estudos_Java/src/Curso_Dojo/Testes.java
@@ -0,0 +1,48 @@
+package Curso_Dojo;
+import java.util.Scanner;
+
+/* REVER O CÓDIGO PARA PROSSEGUIR APENAS COM NÚMERO DE PRODUTOS ACEITO NO SISTEMA*/
+
+
+public class Testes {
+    public static void main(String[] args) {
+        //Sistema de caixa:
+        int qtdProduto;
+        final float valorProd = 1.99f;
+        float resultado;
+        Scanner scan = new Scanner(System.in);
+        boolean keepBuying = false;
+
+        //Inserção dos pedidos
+        System.out.println("Digite a quantidade de produtos: ");
+        qtdProduto = Integer.parseInt(scan.next());
+
+        if(qtdProduto > 100) {
+            System.out.println("Valor inválido de Compra");
+            qtdProduto =  0;
+        }
+
+        //Loop para acréscimo ou continuação do sistema
+        do {
+            System.out.println("Deseja adicionar algo mais ao carrinho?");
+            String ContinuaCompra = scan.next();
+
+            //Teste de condição para continuar compra
+            if(ContinuaCompra.equalsIgnoreCase("s")) {
+                System.out.println("Digite a quantidade a mais: ");
+                int acresProd = Integer.parseInt(scan.next());
+                qtdProduto = acresProd + qtdProduto;
+                    if(qtdProduto > 100) {
+                        System.out.println("Valor inválido de Compra");
+                    }
+            } else {
+                keepBuying = true;
+            }
+        } while (!keepBuying);
+
+        //Cálculo do preço e mostrando resultados
+        resultado = qtdProduto * valorProd;
+        System.out.println("A quantidade total de Itens: " + qtdProduto);
+        System.out.printf("%nO valor total da Compra é: %.2f", resultado);
+    }
+}
--- Estudos_Java/src/Loiane_CursoJava/Exercicios/Exerc17.java
+++ Estudos_Java/src/Loiane_CursoJava/Exercicios/Exerc17.java
@@ -3,26 +3,41 @@ import java.util.Scanner;
 
 public class Exerc17 {
     public static void main(String[] args) {
-        //29. Consulta de preço fixo
+          //29. Consulta de preço fixo
         Scanner scan = new Scanner(System.in);
+//
+//        //Inserindo valores padrão:
+//        float[] valores = new float[50];
+//        valores[0] = 1.99f;
+//
+//        System.out.println("Digite a quantidade de produtos: ");
+//        int qtdProduto = Integer.parseInt(scan.nextLine());
+//
+//        for(int i = 1; i<50; i++) {
+//            valores[i] = valores[0] * i;
+//
+//            if(qtdProduto == i){
+//                System.out.printf("%nO valor a ser pago é de R$ %.2f reais", valores[i]);
+//            } else if (qtdProduto > 50) {
+//                System.out.println("Valor fora da tabela");
+//                System.exit(0);
+//            }
+//        }
+//        scan.close();
 
-        //Inserindo valores padrão:
-        float[] valores = new float[50];
-        valores[0] = 1.99f;
+        //32. Calculando fatorial:
+        System.out.println("Digite o Número para fatorial:");
+        int n = scan.nextInt();
 
-        System.out.println("Digite a quantidade de produtos: ");
-        int qtdProduto = Integer.parseInt(scan.nextLine());
+        //criando o Array para o fatorial
+        int[] num1 = new int[n];
+        int result = 1;
 
-        for(int i = 1; i<50; i++) {
-            valores[i] = valores[0] * i;
-
-            if(qtdProduto == i){
-                System.out.printf("%nO valor a ser pago é de R$ %.2f reais", valores[i]);
-            } else if (qtdProduto > 50) {
-                System.out.println("Valor fora da tabela");
-                System.exit(0);
-            }
+        //Loop que vai fatorando os elementos do Array
+        for (int i = n; i > 0; i--) {
+            result *= i;
+            System.out.printf("%nFatorial de %d * %d = %d", n, i, result);
         }
-        scan.close();
+        System.out.printf("%nO fatorial Total de %d é %d", n, result);
     }
 }
