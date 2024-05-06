ff8b61b2628e59fd6d67a6b82ada569f14355d35
Matheus A <matheusalvessouza1@hotmail.com>
2024-05-05 23:24:11 -0300

    Update 05_05
    Vou tentar avançar umas 15 aula de madrugada pra tentar voltar ao ritmo e pegar aulas que eu goste mais de estudar

---
+++ Estudos_Java/src/Curso_Dojo/OOP/UtilityClasses/Dominio/Strings02.java
@@ -0,0 +1,9 @@
+package Curso_Dojo.OOP.UtilityClasses.Dominio;
+
+public class Strings02 {
+    public static void main(String[] args) {
+        String nome = "Luis";
+        System.out.println(nome);
+
+    }
+}
