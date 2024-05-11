842e42b27200f5da4c1ffcd88a1ab8796d510ea6
Matheus A <matheusalvessouza1@hotmail.com>
2024-05-10 23:40:23 -0300

    Update 10_05
    Preciso revisar depois as datas do Java.

--- Estudos_Java/src/Curso_Dojo/OOP/UtilityClasses/Dominio/TimeClasses/TemporalAdjustersClass.java
+++ Estudos_Java/src/Curso_Dojo/OOP/UtilityClasses/Dominio/TimeClasses/TemporalAdjustersClass.java
@@ -1,12 +1,15 @@
 package Curso_Dojo.OOP.UtilityClasses.Dominio.TimeClasses;
 
-import Curso_Dojo.Atividades.ExercAssociation.Domain.Local;
-
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 
 public class TemporalAdjustersClass {
+
+    /*
+    Retorne a Aula 127 para fazer o exercício de data!.
+    */
+
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
         now = now.withDayOfMonth(20);
@@ -14,6 +17,8 @@ public class TemporalAdjustersClass {
         //Contando com a data de agora, o TemporalAdjusters me mostra quando será a próxima quarta
         now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.WEDNESDAY));
         System.out.println(now);
+
+
 
     }
 }
